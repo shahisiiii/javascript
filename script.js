e32bff00b7fa4531af077d99b3add74184550c40
jabeel shahis ak <jabeelshahisak@gmail.com>
2023-11-20 22:32:07 +0530

    index1 created and new function created

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-console.log("welcome to javascripts")
-window.alert("alert box")
-document.write("from write method")
-document.getElementById("id1").innerHTML="javascript2"
+// console.log("welcome to javascripts")
+// window.alert("alert box")
+// document.write("from write method")
+// document.getElementById("id1").innerHTML="javascript2"
+
+function hello(){
+    document.write("welcome to java script")
+}
