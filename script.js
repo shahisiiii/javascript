3aceafb75441cea861c1b9d9d706e75f3425073f
jabeel shahis ak <jabeelshahisak@gmail.com>
2023-11-21 10:09:13 +0530

    data types in Java scripts

--- script.js
+++ script.js
@@ -7,11 +7,52 @@ function hello(){
     document.write("welcome to java script")
 }
 
-var x=3
+// var x=3
 
-{
-    x=5
-    document.write("inside block:"+x+"<br>")
-}
+// {
+//     x=5
+//     document.write("inside block:"+x+"<br>")
+// }
 
-document.write("inside block:"+x+"<br>")
+// document.write("inside block:"+x+"<br>")
+
+
+//  let x=3  //re-declairation not possible
+
+// {
+//     let x=5
+//     document.write("inside block:"+x+"<br>")
+// }
+
+// document.write("inside block:"+x+"<br>")
+
+// const x=3
+
+// {
+//     x=5
+//     document.write("inside block:"+x+"<br>")
+// }
+
+// document.write("inside block:"+x+"<br>")
+
+
+// data types in java scripts
+
+
+x=10.44
+document.write(typeof(x)+"<br>")
+
+s="java scripts"
+document.write(typeof(s)+"<br>")
+
+b=true
+document.write(typeof(b)+"<br>")
+
+var z 
+document.write(typeof(z)+"<br>")
+
+i=[1,2,3,"hello"]
+document.write(typeof(i)+"<br>")
+
+d={"name":"shahis","age":"kk"}
+document.write(typeof(d)+"<br>")
