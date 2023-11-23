f985496042ef1601d53e11cc95d849cad602a74b
jabeel shahis ak <jabeelshahisak@gmail.com>
2023-11-23 09:47:22 +0530

    switch condition

--- script.js
+++ script.js
@@ -112,3 +112,15 @@
 //         default_statement
 
 // }
+
+x=1
+switch (x){
+    case 1:
+        console.log("value is 1")
+        break;
+    case 2:
+        console.log("value is 2")
+        break;
+    default:
+        console.log("some other number")
+}
